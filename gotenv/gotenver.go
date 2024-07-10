a4d98aaaaf95b86951f86b43e8ac249931e4c265
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-10 17:53:02 +0600

    main processing

---
+++ gotenv/gotenver.go
@@ -0,0 +1,32 @@
+package gotenv
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/OlyMahmudMugdho/gotenv/parser"
+)
+
+func GetFromEnv() map[string]string {
+	vars := make(map[string]string)
+
+	data, err := os.ReadFile(".env")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(string(data)))
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		var parts []string = parser.ParseLine(line)
+		var key string = strings.TrimSpace(parts[0])
+		var value string = strings.TrimSpace(parts[1])
+		vars[key] = value
+	}
+
+	return vars
+}
