bf96eb62080d01bd56faa1563c1bad94c108e04e
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-10 17:53:30 +0600

    checked everything- done

--- main.go
+++ main.go
@@ -1,30 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
 
-	"github.com/OlyMahmudMugdho/gotenv/parser"
+	"github.com/OlyMahmudMugdho/gotenv/gotenv"
 )
 
 func main() {
-	vars := make(map[string]string)
-
-	data, err := os.ReadFile(".env")
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	scanner := bufio.NewScanner(strings.NewReader(string(data)))
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		var parts []string = parser.ParseLine(line)
-		var key string = strings.TrimSpace(parts[0])
-		var value string = strings.TrimSpace(parts[1])
-		vars[key] = value
-	}
+	var envs map[string]string = gotenv.GetFromEnv()
+	fmt.Println(envs["NAME"])
 }
